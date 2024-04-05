3afe061530729a1975d3a6013feedd4f9ea3d92d
chanhalee <chan771788@gmail.com>
2024-04-05 18:41:52 +0900

    refactor
    Number#numberInstances 를 한번에 초기화하는 메서드 구현

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -7,17 +7,28 @@ import java.util.List;
 import java.util.Objects;
 
 public class Number {
-    private static Number[] numberInstances = new Number[NUMBER_MAX];
+    private static Number[] numberInstances = null;
     private final int number;
     private Number(int number) {
         this.number = number;
     }
     public static Number getNumberInstance(int number) {
+        if (numberInstances == null) {
+            numberInstances = new Number[NUMBER_MAX - NUMBER_MIN + 1];
+            initiateNumberInstances();
+        }
+
         validateBall(number);
         if (numberInstances[number - 1] == null) {
             numberInstances[number - 1] = new Number(number);
         }
         return numberInstances[number - 1];
+    }
+
+    private static void initiateNumberInstances() {
+        for (int i = NUMBER_MIN; i < NUMBER_MAX+1; i++) {
+            numberInstances[i - 1] = new Number(i);
+        }
     }
 
     private static void validateBall(int number) {
